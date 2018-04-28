baffd8a3a49c5f35fd9acfb849b1d094d9cbf12a
Liz Zitnik <lizbiz@Lizs-MacBook-Pro.local>
2018-04-27 17:30:21 -0700

    rewriting tests

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ exports.DATABASE_URL =
   // process.env.DATABASE_URL ||
   // global.DATABASE_URL ||
   "mongodb://localhost/good-dog"
+exports.DATABASE_TEST_URL = "mongodb://localhost/good-dog-test"
 exports.PORT = process.env.PORT || 8080
 exports.JWT_SECRET = process.env.JWT_SECRET
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d"
--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,9 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bodyParser = require("body-parser")
+const jsonParser = bodyParser.json()
 
 const {
   app,
@@ -14,7 +17,7 @@ const {
   Comments
 } = require('../models');
 const {
-  DATABASE_URL,
+  DATABASE_TEST_URL,
   JWT_SECRET
 } = require('../config')
 
@@ -23,31 +26,37 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Dog', function() {
+
+  const username = 'exampleUser';
+  const password = 'examplePass';
+  const firstName = 'Example';
+  const lastName = 'User';
+
   before(function() {
-    return runServer(DATABASE_URL);
+    return runServer(DATABASE_TEST_URL);
   });
-
-  beforeEach('Dog', function() {
-
-  })
 
   after(function() {
     return closeServer();
   });
-  describe('GET endpoint', function() {
-    let token;
-    beforeAll(() => {
-      // Setup a user
+
+  beforeEach(function() {
+    return User.hashPassword(password).then(password =>
       User.create({
-        username: "test",
-        password: "12345678",
-        name: "Test"
-      }).then(user => {
-        // make a token
-        token = jwt.sign(user.serialize(), JWT_SECRET)
-        token = `JWT ${token}`
+        username,
+        password,
+        firstName,
+        lastName
       })
-    })
+    );
+  });
+
+  afterEach(function() {
+    return User.remove({});
+  });
+
+
+  describe('GET endpoint', function() {
 
     it('should return all existing dogs', function() {
       let res;
@@ -61,9 +70,23 @@ describe('Dog', function() {
     });
 
     it('should return all my dogs', function() {
+      const token = jwt.sign({
+          user: {
+            username,
+            firstName,
+            lastName
+          }
+        },
+        JWT_SECRET, {
+          algorithm: 'HS256',
+          subject: username,
+          expiresIn: '7d'
+        }
+      );
       let res;
       return chai.request(app)
         .get('/dogs/my')
+        .set('authorization', `Bearer ${token}`)
         .then(function(_res) {
           res = _res;
           expect(res).to.have.status(200);
@@ -84,6 +107,20 @@ describe('Dog', function() {
 
   describe('POST endpoints', function() {
     it('should add a new dog', function() {
+      const token = jwt.sign({
+          user: {
+            username,
+            firstName,
+            lastName
+          }
+        },
+        JWT_SECRET, {
+          algorithm: 'HS256',
+          subject: username,
+          expiresIn: '7d'
+        }
+      );
+
       const newDog = {
         dogName: 'Morgan',
         dogBreed: 'Hound',
@@ -93,22 +130,45 @@ describe('Dog', function() {
       return chai.request(app)
         .post('/dogs')
         .send(newDog)
+        .set('authorization', `Bearer ${token}`)
         .then(function(res) {
           expect(res).to.have.status(201);
         });
     });
 
-    it('should add a new user', function() {
-      const newUser = {
-        username: 'mcgill',
-        password: 'doggo45'
-      }
+    it.only('should add a new user', function() {
 
       return chai.request(app)
         .post('/users')
-        .send(newUser)
-        .then(function(res) {
+        .send({
+          username,
+          password,
+          firstName,
+          lastName
+        })
+        .then(res => {
           expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys(
+            'username',
+            'firstName',
+            'lastName'
+          );
+          expect(res.body.username).to.equal(username);
+          expect(res.body.firstName).to.equal(firstName);
+          expect(res.body.lastName).to.equal(lastName);
+          return User.findOne({
+            username
+          });
+        })
+        .then(user => {
+          expect(user).to.not.be.null;
+          expect(user.firstName).to.equal(firstName);
+          expect(user.lastName).to.equal(lastName);
+          return user.validatePassword(password);
+        })
+        .then(passwordIsCorrect => {
+          expect(passwordIsCorrect).to.be.true;
         });
     })
 
@@ -128,9 +188,18 @@ describe('Dog', function() {
   })
 
   describe('PUT endpoint', function() {
+    before(function() {
+      Dog.create({
+        dogName: 'Borgan',
+        dogBreed: 'Hound',
+        symptom: 'Dry Nose',
+        additionalInfo: 'blah blah'
+      })
+    })
     it('should update dogs on PUT', function() {
       const updateData = {
-        attending: 2
+        symptom: 'whatevs',
+        additionalInfo: 'yeah yeah'
       };
 
       return Dog
@@ -142,7 +211,8 @@ describe('Dog', function() {
             .send(updateData);
         })
         .then(function(res) {
-          expect(res).to.have.status(204);
+          expect(res).to.have.status(201);
+          expect(res.body.symptom).to.equal('whatevs');
         });
 
     });
